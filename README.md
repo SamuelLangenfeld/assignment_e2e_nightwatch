911d514d8106755ec0197c3efef41d2ba5d7d170
SamuelLangenfeld <samlangenfeld@gmail.com>
2017-11-28 19:20:10 -0500

    final

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 # assignment_e2e_nightwatch
 
 Assignment - Viking Code School exploration via e2e tests with Nightwatch
+=======
+# nightwatch_base
+
+A starter repo for implementing Nightwatch.js e2e tests.
+>>>>>>> af70cbea0a3d9ab4878e4bdb021ceb1d1e144051
--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,18 +1,37 @@
+<<<<<<< HEAD
 const path = require('path');
 const serverPath = path.resolve('./bin/selenium-server-standalone-3.4.0.jar');
 
 module.exports = {
   src_folders: ['tests'],
   output_folder: 'reports',
+=======
+const path = require("path");
+const serverPath = path.resolve("./bin/selenium-server-standalone-3.7.1.jar");
+
+module.exports = {
+  src_folders: ["tests"],
+  output_folder: "reports",
+>>>>>>> af70cbea0a3d9ab4878e4bdb021ceb1d1e144051
 
   selenium: {
     start_process: true,
     server_path: serverPath,
+<<<<<<< HEAD
     port: 4444
+=======
+    port: 4444,
+    cli_args: {
+      // Uncomment this option only if you downloaded
+      // the driver in binary form
+      // "webdriver.chrome.driver" : "./bin/chromedriver.exe"
+    }
+>>>>>>> af70cbea0a3d9ab4878e4bdb021ceb1d1e144051
   },
 
   test_settings: {
     default: {
+<<<<<<< HEAD
       launch_url: 'http://localhost:3000',
       selenium_port: 4444,
       selenium_host: 'localhost',
@@ -20,12 +39,25 @@ module.exports = {
       screenshots: {
         enabled: true,
         path: 'screenshots'
+=======
+      launch_url: "http://www.vikingcodeschool.com",
+      selenium_port: 4444,
+      selenium_host: "localhost",
+      silent: true,
+      screenshots: {
+        enabled: true,
+        path: "screenshots"
+>>>>>>> af70cbea0a3d9ab4878e4bdb021ceb1d1e144051
       }
     },
 
     chrome: {
       desiredCapabilities: {
+<<<<<<< HEAD
         browserName: 'chrome'
+=======
+        browserName: "chrome"
+>>>>>>> af70cbea0a3d9ab4878e4bdb021ceb1d1e144051
       }
     }
   }
